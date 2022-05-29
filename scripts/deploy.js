eac24c6fe972f2c9e446927a08fa1a859d8366c7
Kourouta <106455651+Kourouta@users.noreply.github.com>
2022-05-29 02:44:55 +0200

    Update deploy.js
    Constructor requires an address.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 async function main() {
     // We get the contract to deploy
     const MyFirstContract = await ethers.getContractFactory("MyFirstContract");
-    const myFirstContract = await MyFirstContract.deploy();
+    const myFirstContract = await MyFirstContract.deploy('0x9326BFA02ADD2366b30bacB125260Af641031331');
 
     console.log("Contract deployed to:", myFirstContract.address);
   }
